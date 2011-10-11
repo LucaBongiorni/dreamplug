e2643a53965345412126693cd974b28e19499c76
Andre Erdmann <dywi@mailerd.de>
2011-10-11 20:40:36 +0200

    "global" actions in led_set.pl

--- scripts/led_set.pl
+++ scripts/led_set.pl
@@ -7,7 +7,6 @@ package led_control;
 #my $led_root = '/sys/devices/platform/leds-gpio/leds';
 my $led_root = '/sys/class/leds';
 my $led_ctrl = "brightness";
-my $s = '/';
 
 my %led_map = (
 	power => 'guruplug:red:health',
@@ -16,21 +15,20 @@ my %led_map = (
 	bt    => 'guruplug:green:health',
 );
 
+# power seems to be always on (even with brightness = 0 and trigger = none) and won't be controlled here
 my @control = ("ap", "wifi", "bt");
 
-my %circle = (
-	ap => "wifi",
+my %rainbow_circle = (
+	ap   => "wifi",
 	wifi => "bt",
-	bt => "ap",
+	bt   => "ap",
 );
 
 # hard value
 my $max = 255;
 
 my %led = ();
-foreach my $l (@control) {
-	$led {$l} = $led_root . $s . $led_map {$l} . $s . $led_ctrl;
-}
+foreach my $l (@control) { $led {$l} = $led_root . '/' . $led_map {$l} . '/' . $led_ctrl; }
 
 # led shortcuts
 $led {'vdr'} = $led {'bt'};
@@ -42,33 +40,28 @@ $led {'1'} = $led {'ap'};
 $led {'2'} = $led {'bt'};
 
 # on (led_path)
-sub on() {
-#	die "bad led: $_[0]" unless (-w $_[0]);
-	system ("echo $max > $_[0] 2>/dev/null");
-}
-
-sub off() {
-#	die "bad led: $_[0]" unless (-w $_[0]);
-	system ("echo 0 > $_[0] 2>/dev/null");
-}
-
-sub set() {
-	system ("echo $_[1] > $_[0] 2>/dev/null");
-}
-
-sub print_status() {
-#	die "bad led: $_[0]" unless (-w $_[0]);
-	print &status ($_[0]) . "\n";
-}
+sub on()  { system ("echo  $max > $_[0] 2>/dev/null"); }
+sub off() { system ("echo     0 > $_[0] 2>/dev/null"); }
+sub set() { system ("echo $_[1] > $_[0] 2>/dev/null"); }
 
 sub status() {
-#	die "bad led: $_[0]" unless (-w $_[0]);
 	my $l = $_[0];
 	open (RR, "<", $l) or die $!;
 	my $out = <RR>;
 	close (RR);
 	chomp ($out);
 	$out;
+}
+sub status_all() {
+	my %status = ();
+	foreach my $l (@control) { $status {$l} = &status ($led {$l}); }
+	%status;
+}
+
+sub print_status() { print &status ($_[0]) . "\n"; }
+sub print_status_all() {
+	my %status = &status_all();
+	foreach my $l (@control) { print $l . ': ' . $status {$l} . qq<\n>; }
 }
 
 sub increase() {
@@ -77,7 +70,7 @@ sub increase() {
 	if ($s < $max) {
 		$s++;
 		&set ($l, $s);
-	}
+	} else { 0; }
 }
 sub decrease() {
 	my $l = $_[0];
@@ -85,11 +78,10 @@ sub decrease() {
 	if ($s > 0) {
 		$s--;
 		&set ($l, $s);
-	}
+	} else { 0; }
 }
 
 sub toggle() {
-#	die "bad led: $_[0]" unless (-w $_[0]);
 	my $l = $_[0];
 	my $s = &status ($l);
 	if ($s) {
@@ -106,7 +98,8 @@ sub list() {
 }
 
 sub rainbow() {
-	0;
+	# TODO
+	127;
 }
 
 sub ledcheck() {
@@ -129,8 +122,6 @@ my %action = (
 	off      => \&off,
 	toggle   => \&toggle,
 	status   => \&print_status,
-	rainbow  => \&rainbow,
-	list     => \&list,
 	increase => \&increase,
 	decrease => \&decrease,
 
@@ -147,24 +138,40 @@ my %action = (
 	3 => \&print_status,
 	s => \&print_status,
 
-	4 => \&rainbow,
-	r => \&rainbow,
-
 	"+" => \&increase,
 	"-" => \&decrease,
 
 );
+my %global_action = (
+	status   => \&print_status_all,
+	rainbow  => \&rainbow,
+	list     => \&list,
 
+	# shortcuts
+	4 => \&rainbow,
+	r => \&rainbow,
+	s => \&print_status_all,
+);
 
-die "usage: led_set.pl <dev> <action>" if (scalar (@ARGV) != 2);
+die "usage: led_set.pl <dev|-> <action|global_action>" if (scalar (@ARGV) != 2);
 die "please specifiy a LED to control." if ($ARGV [0] eq '');
 die "please specify an action." if ($ARGV [1] eq '');
 
-die "bad LED : " . $ARGV [0] unless (exists $led {$ARGV [0]});
-my $dev = $led {$ARGV [0]};
-die "bad LED file : $dev" unless (&ledcheck ($dev));
 
-die "bad action : " . $ARGV [1] unless (exists $action {$ARGV [1]});
-my $action = $action {$ARGV [1]};
 
-&$action ($dev);
+my ($dev,$action);
+if (($ARGV [0] eq q<all>) || ($ARGV [0] eq q<->)) {
+	$dev = "undef";
+	die "bad global action : " . $ARGV [1] unless (exists $global_action {$ARGV [1]});
+	$action = $global_action {$ARGV [1]};
+} else {
+	$dev = $led {$ARGV [0]};
+	die "bad LED : " . $ARGV [0] unless (exists $led {$ARGV [0]});
+	die "bad LED file : $dev" unless (&ledcheck ($dev));
+	die "bad action : " . $ARGV [1] unless (exists $action {$ARGV [1]});
+	$action = $action {$ARGV [1]};
+}
+
+
+my $ret = &$action ($dev);
+exit ($ret) if ($ret);
